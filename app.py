16bd43e2442cb412a56d621a3f517742bf8a6abd
RaphaFang <fangsihyu0211@gmail.com>
2024-05-28 11:10:18 +0800

    setup /api/mrts

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
 from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 app=FastAPI()
+
+import mysql.connector
+import os
+sql_password = os.getenv('SQL_PASSWORD')
+sql_username = os.getenv('SQL_USER')
+db_config = {
+    'host': 'localhost',
+    'user': sql_username,
+    'password': sql_password,
+    'database': 'basic_db',
+}
+
+
+@app.get("/api/mrts")
+def api_mrts():
+    try:
+        # raise mysql.connector.Error("Manually triggered error for testing")
+        mydb = mysql.connector.connect(**db_config)
+        cursor = mydb.cursor()
+        cursor.execute("SELECT mrt, COUNT(*) as count FROM processed_data GROUP BY mrt ORDER BY count DESC;") 
+        mrts_counted = cursor.fetchall()
+        return [n[0] for n in mrts_counted]
+    except mysql.connector.Error as err:
+        return JSONResponse(    
+            status_code=500,
+            content={"error": True, "message": str(err)}
+        )
+    finally:
+        cursor.close()
+        mydb.close()
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
