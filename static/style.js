25b5344b3c0fc1487771ae76a0b7af92dcc3d5ef
RaphaFang <fangsihyu0211@gmail.com>
2024-06-06 13:11:55 +0800

    update the observer

--- static/style.js
+++ static/style.js
@@ -1,80 +1,85 @@
 "use strict";
-
+let nextPage = 0;
 document.addEventListener("DOMContentLoaded", async function () {
-  console.log("start fetching the first page, fetching start...");
+  console.log(">>>>>>>> fetching start...");
 
   try {
     const url = `http://52.4.229.207:8000/api/attractions?page=0`;
-    console.log("Fetching URL: ", url);
-
-    let response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
+    let response = await fetch(url);
     console.log("Response status: ", response.status);
-    console.log("Response ok: ", response.ok);
-
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
     }
-
     let data = await response.json();
-    console.log("Fetched data: ", data);
-    displayData(data);
+    console.log("Fetched data: ", data["data"]);
+    displayHtml(data["data"]);
+    nextPage = data["nextPage"];
+    console.log(nextPage);
+
+    // 建立一個觀察者， IntersectionObserver ，檢測有沒有拱動到特定的 div
+    const observer = new IntersectionObserver(loadMore, {
+      root: null, // 視窗
+      rootMargin: "0px",
+      threshold: 1.0, // 完全可見時觸發，調用下方的 addUpFetch()
+    });
+    observer.observe(document.getElementById("load-more-trigger")); // 開始觀察
   } catch (error) {
     console.error("Fetch error: ", error);
   }
-  console.log("fetching the first page, fetching end...");
+  console.log(">>>>>>>> fetching end...");
 });
 
-function displayData(data) {
-  const contentDiv = document.getElementById("content");
-  contentDiv.innerHTML = "";
-
-  if (data && data.data) {
-    data.data.forEach((item) => {
-      const div = document.createElement("div");
-      div.innerText = `Name: ${item.name}, Category: ${item.category}`;
-      contentDiv.appendChild(div);
-    });
-  } else {
-    contentDiv.innerText = "No data found";
+function displayHtml(data) {
+  let attracDiv = document.getElementById("attracDiv");
+  // attracDiv.innerHTML = "";
+  for (let n = 0; n < data.length; n++) {
+    // console.log(`Adding item ${n}: `, data[n]["images"][0]);
+    attracDiv.innerHTML += `
+      <div class="background-image-container">
+        <div class="first-part">
+          <div class="attraction-pic" id="attraction-pic-${n}">
+            <img
+              src="${data[n]["images"][0]}"
+              alt="Attraction Image"
+              class="attraction-pic-hidden lazy"
+            />
+          </div>
+          <div class="attraction-name-div">
+            <div class="attraction-name">${data[n]["name"]}</div>
+          </div>
+        </div>
+        <div class="third-part">
+          <div class="mrt-name">${data[n]["mrt"]}</div>
+          <div class="tag-name">${data[n]["category"]}</div>
+        </div>
+      </div>
+    `;
   }
 }
 
-// function displayAttracItem(data) {
-//   let attracDiv = document.getElementById("attracDiv");
-//   attracDiv.innerHTML = `
-//           <p>${data["name"]}(${data["username"]})</p>
+async function addUpFetch(page) {
+  const url = `http://52.4.229.207:8000/api/attractions?page=${page}`;
+  let response = await fetch(url);
+  console.log("Response status: ", response.status);
+  if (!response.ok) {
+    throw new Error("Network response was not ok " + response.statusText);
+  }
+  let data = await response.json();
+  console.log("Fetched data: ", data["data"]);
+  displayHtml(data["data"]);
+  nextPage = data["nextPage"];
+  console.log(nextPage);
+}
 
-//         <div class="title-1 background-image-container">
-//           <div class="first-part">
-//             <div class="attraction-pic" id="attraction-pic">
-//               <img
-//                 src="https://s3-alpha-sig.figma.com/img/cf92/70bb/362e0c2e64c0c59874238a0fcef074d9?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SzuQSbIeR1oaCsrBc91Pv5WRtl3EOXoLI4Bw~LL8BT92ehkRV6~JY57Vbdx3RXH9AsQr9zgHZWI7-o1t3GIHxgf~3hO3tuPslH6wiLKqRXmV-k5YxjxceOBe~yp3sHX-JZHWpg2LT1lsRhpWaHKOk9Cn2XUkdK33FuBGLeZx3osRvItsMB-R~35y9n5sMk2gfSfyz7FWGFg9nVzOVSbcC0QtzSmKTDLoFh09LZ~p-Pa59GWnk9c9xR8GTNoCDlqu-viHwx4VUFNfKo6gc01hshz8WhIo7DIYu2fKegc7QLrZ34wIdRD-9Y7DI2n0UDBnCaLxV7PazlF1aToBnuPNrg__"
-//                 alt="Attraction Image"
-//                 class="attraction-pic-hidden"
-//               />
-//             </div>
-//             <div class="attraction-name-div">
-//               <div class="attraction-name">平安中</div>
-//             </div>
-//           </div>
-//           <div class="third-part">
-//             <div class="mrt-name">忠孝復興</div>
-//             <div class="tag-name">公共藝術</div>
-//           </div>
-//         </div>
-//       `;
-// }
-
-//   if (data["data"] === null) {
-//     userInfoDiv.innerHTML = "<p>No Data.</p>";
-//   } else {
-//     userInfoDiv.innerHTML = `
-//           <p>${data["name"]}(${data["username"]})</p>
-//       `;
-//   }
+function loadMore(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target); // 停止觀察當前元素
+      addUpFetch(nextPage).then(() => {
+        if (nextPage !== null) {
+          observer.observe(document.getElementById("load-more-trigger")); // 重新啟動observer，觀察新元素
+        }
+      });
+    }
+  });
+}
