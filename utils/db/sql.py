3cdf60a1e79ef3279f58f6eb97ad9679364d0b1f
RaphaFang <fangsihyu0211@gmail.com>
2024-07-01 00:25:25 +0800

    sql pool

--- utils/db/sql.py
+++ utils/db/sql.py
@@ -5,12 +5,12 @@ def sql_pool_buildup():
     pool_config = {
         'pool_name': 'day_trip_pool',
         'pool_size': 10,
-        # 'host': '52.4.229.207',
-        # 'user': os.getenv('SQL_USER'),
-        # 'password': os.getenv('SQL_PASSWORD'),
-        'host': 'localhost',
-        'user': os.getenv('SQL_USER_LOCAL'),
-        'password': os.getenv('SQL_PASSWORD_LOCAL'),
+        'host': '52.4.229.207',
+        'user': os.getenv('SQL_USER'),
+        'password': os.getenv('SQL_PASSWORD'),
+        # 'host': 'localhost',
+        # 'user': os.getenv('SQL_USER_LOCAL'),
+        # 'password': os.getenv('SQL_PASSWORD_LOCAL'),
         'database': 'basic_db',
         'port': 3306,
         'use_pure': True
