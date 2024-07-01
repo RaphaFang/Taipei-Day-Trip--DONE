3200d53334358846322db7d38e317f174c36469f
RaphaFang <fangsihyu0211@gmail.com>
2024-07-02 00:39:05 +0800

    the sql db setup

--- utils/db/sql.py
+++ utils/db/sql.py
@@ -5,17 +5,17 @@ def sql_pool_buildup():
     pool_config = {
         'pool_name': 'day_trip_pool',
         'pool_size': 10,
-        # 'host':"database-v5.cxu0oc6yqrfs.us-east-1.rds.amazonaws.com",
-        # 'user':os.getenv('SQL_USER'),
-        # 'password':os.getenv('SQL_PASSWORD'),
+        'host':"database-v5.cxu0oc6yqrfs.us-east-1.rds.amazonaws.com",
+        'user':os.getenv('SQL_USER'),
+        'password':os.getenv('SQL_PASSWORD'),
         # -----------------------------------------
         # 'host': '52.4.229.207',
         # 'user': os.getenv('SQL_USER'),
         # 'password': os.getenv('SQL_PASSWORD'),
         # -----------------------------------------
-        'host': 'localhost',
-        'user': os.getenv('SQL_USER_LOCAL'),
-        'password': os.getenv('SQL_PASSWORD_LOCAL'),
+        # 'host': 'localhost',
+        # 'user': os.getenv('SQL_USER_LOCAL'),
+        # 'password': os.getenv('SQL_PASSWORD_LOCAL'),
             
         'database': 'basic_db',
         'port': 3306,
