2acd49822478bc7d9d8c579cd1d10dd592f355a2
RaphaFang <fangsihyu0211@gmail.com>
2024-07-05 11:56:33 +0800

    sql

--- utils/db/sql.py
+++ utils/db/sql.py
@@ -6,13 +6,13 @@ import asyncio
 async def build_async_sql_pool():
     return await aiomysql.create_pool(
         # -----------------------------------------
-        # host="database-v5.cxu0oc6yqrfs.us-east-1.rds.amazonaws.com",
-        # user=os.getenv('SQL_USER'),
-        # password=os.getenv('SQL_PASSWORD'),
+        host="database-v5.cxu0oc6yqrfs.us-east-1.rds.amazonaws.com",
+        user=os.getenv('SQL_USER'),
+        password=os.getenv('SQL_PASSWORD'),
         # -----------------------------------------
-        host='localhost',
-        user=os.getenv('SQL_USER_LOCAL'),
-        password=os.getenv('SQL_PASSWORD_LOCAL'),
+        # host='localhost',
+        # user=os.getenv('SQL_USER_LOCAL'),
+        # password=os.getenv('SQL_PASSWORD_LOCAL'),
         # -----------------------------------------
         port=3306,
         db='basic_db',
