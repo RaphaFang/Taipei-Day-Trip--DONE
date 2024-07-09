713ab9fc1dc78a5669d6fa2ac95ca798d6636e4e
RaphaFang <fangsihyu0211@gmail.com>
2024-07-09 21:04:37 +0800

    forget the middleware

--- utils/auth_middleware.py
+++ utils/auth_middleware.py
@@ -6,7 +6,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         if request.url.path.startswith("/static/") or request.url.path.startswith("/api") :
             return await call_next(request)
-        if request.url.path.startswith("/attraction/") :
+        if request.url.path.startswith("/attraction/") or request.url.path.startswith("/auth/") : 
             return await call_next(request)
         if request.url.path not in ["/", "/booking", "/thankyou"]:
             return RedirectResponse(url='/')
