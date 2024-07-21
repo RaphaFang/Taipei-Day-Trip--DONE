c5b5f1ca6749be222e9750f7a1f06e48ff83284f
RaphaFang <fangsihyu0211@gmail.com>
2024-07-21 16:06:55 +0800

    delete comments

--- utils/db/redis.py
+++ utils/db/redis.py
@@ -9,14 +9,3 @@ async def build_async_redis_pool():
         max_connections=10
     )
     return r_pool
-
-# def redis_pool_buildup():
-#     r_pool = redis.ConnectionPool(
-#         host='localhost',
-#         port=6379,
-#         db=0,
-#         password=os.getenv('REDIS_PASSWORD',None),
-#         max_connections=10 
-#     )
-#     return r_pool
-
