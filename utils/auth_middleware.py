20d97aa890335f3d3b83aee2cd489a1825a63637
RaphaFang <fangsihyu0211@gmail.com>
2024-07-22 00:10:11 +0800

    fix: the middleware setup

--- utils/auth_middleware.py
+++ utils/auth_middleware.py
@@ -8,6 +8,6 @@ class AuthMiddleware(BaseHTTPMiddleware):
             return await call_next(request)
         if request.url.path.startswith("/tdt/v1/attraction/") or request.url.path.startswith("/tdt/v1/auth/") : 
             return await call_next(request)
-        if request.url.path not in ["/tdt/v1/", "/tdt/v1/booking", "/tdt/v1/thankyou", '/tdt/v1/history_orders', '/tdt/v1/docs']:
+        if request.url.path not in ["/tdt/v1/", "/tdt/v1/booking", "/tdt/v1/thankyou", '/tdt/v1/history_orders', '/tdt/v1/docs', '/tdt/v1/openapi.json']:
             return RedirectResponse(url='/tdt/v1/')
         return await call_next(request)
